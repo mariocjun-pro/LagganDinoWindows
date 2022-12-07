3f00039e3a352e944c1f2d937c27166cd42fb1e7
Mário Cordeiro Junior <mario.2018@alunos.utfpr.edu.br>
2022-12-07 16:19:10 -0300

    Melhorando script

--- run.ps1
+++ run.ps1
@@ -1,26 +1,44 @@
+Install-Module -Name WriteAscii -Force
+Import-Module WriteAscii
+Write-Host "====================================================================================="
+Write-Ascii "LagganDino" -fore rainbow
+
 $dir = Get-Location
-Write-Host "Verificando se a pasta build existe..."
+Write-Host "====================================================================================="
+Write-Host "|> Diretorio atual: $dir"
+
+Write-Host "====================================================================================="
+Write-Host "|> Verificando se a pasta build existe..."
 Test-Path $dir\build | Out-Null
 if ($?) {
-    Write-Host "existe, apagando..."
+    Write-Host "====================================================================================="
+    Write-Host "|> A pasta build existe, removendo..."
     Remove-Item -Path $dir\build -Recurse -Force | Out-Null
 }
 
-Write-Host "criando a pasta build..."
+Write-Host "====================================================================================="
+Write-Host "|> Re-criando a pasta build..."
 New-Item -ItemType Directory -Path $dir\build -Force | Out-Null
 
-Write-Host "OK, pasta criada, entrando na pasta build..."
+Write-Host "====================================================================================="
+Write-Host "|> OK, pasta criada, entrando na pasta build..."
 Set-Location $dir\build | Out-Null
 
-Write-Host "Executando o cmake..."
+Write-Host "====================================================================================="
+Write-Host "|> Executando o cmake..."
+Write-Host "====================================================================================="
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Debug
 
-Write-Host "Executando o ninja..."
+Write-Host "====================================================================================="
+Write-Host "|> Executando o ninja..."
+Write-Host "====================================================================================="
 ninja
 
-Write-Host "Executando LagganDino..."
+Write-Host "====================================================================================="
+Write-Host "|> Executando LagganDino, Bom jogo!"
+Write-Host "====================================================================================="
 Set-Location $dir\build
-.\LagganDino.exe
+#.\LagganDino.exe
 
 Set-Location $dir | Out-Null
 
